58b9573a61ab839985c68aa6908efd69a92cbf2a
migurdia <62302391+migurdia97@users.noreply.github.com>
2024-03-03 17:31:54 +0200

    Add files via upload

---
+++ changer.py
@@ -0,0 +1,53 @@
+
+import os
+import random
+import time
+import base64
+from tls_client import Session
+
+ 
+sesh = Session(client_identifier="chrome_115", random_tls_extension_order=True)
+
+PICTURE_FOLDER = 'profile_pictures/'
+
+def get_random_picture():
+    pictures = os.listdir(PICTURE_FOLDER)
+    picture = random.choice(pictures)
+    return os.path.join(PICTURE_FOLDER, picture)
+ 
+token = input("Enter your discord token: ")
+
+ 
+headers = {
+        "authority": "discord.com",
+        "method": "PATCH",
+        "scheme": "https",
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US",
+        "authorization": token,
+        "origin": "https://discord.com",
+        "sec-ch-ua": '"Not/A)Brand";v="99", "Brave";v="115", "Chromium";v="115"',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9020 Chrome/108.0.5359.215 Electron/22.3.26 Safari/537.36",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "X-Debug-Options": "bugReporterEnabled",
+        "X-Discord-Locale": "en-US",
+        "X-Discord-Timezone": "Asia/Calcutta",
+        "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDIwIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDUiLCJvc19hcmNoIjoieDY0IiwiYXBwX2FyY2giOiJpYTMyIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV09XNjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIGRpc2NvcmQvMS4wLjkwMjAgQ2hyb21lLzEwOC4wLjUzNTkuMjE1IEVsZWN0cm9uLzIyLjMuMjYgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6IjIyLjMuMjYiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjoyNDAyMzcsIm5hdGl2ZV9idWlsZF9udW1iZXIiOjM4NTE3LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsLCJkZXNpZ25faWQiOjB9"
+    }
+ 
+payload = {
+    "avatar": f"data:image/jpeg;base64,{base64.b64encode(open(get_random_picture(), 'rb').read()).decode()}"
+}
+while True:
+    r =sesh.patch("https://discord.com/api/v9/users/@me", json=payload, headers=headers)
+    if r.status_code == 200:
+        print("Profile picture changed successfully")
+    else:
+        print(f"Error: {r.status_code}")
+    time.sleep(3600)
+
